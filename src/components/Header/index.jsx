960224de4816a88f08b222ea8318c489e9004b3d
Tamara Haak <132607121+TamaraHaak@users.noreply.github.com>
2024-08-13 21:44:33 +0200

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,1 +1,34 @@
+import React from 'react'
+import logo from './Home 2.ico'
+import s from './index.module.css'
+import { IoMenu } from "react-icons/io5"
+
+
+
+
+
+const Header = ({ scrollToFooter }) => {
+
+
+  return (
+   
+    <header className={[s.header, 'wrapper'].join(' ')}>
+      <img src={logo} alt='logo' />
+      <nav>
+        <li>Haupt</li>
+        <li>Service</li>
+        <li>Bewertungen</li>
+        <li>Кontakte</li>
+        <li>Katalog</li>
+        <li>Uber uns</li>
+        <li>Preis</li>
+      </nav>
+      <div onClick={scrollToFooter}>Termin vereinbaren</div>
+      <IoMenu className={s.menu_icon} />
+    </header>
+   
+  )
+};
+ export default Header;
+
 
